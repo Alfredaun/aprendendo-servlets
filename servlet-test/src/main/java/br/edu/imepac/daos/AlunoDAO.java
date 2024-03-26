4e0348423ad998874400a9e91c0b3993baddd07f
Alfredo <147112705+Alfredaun@users.noreply.github.com>
2024-03-25 21:32:26 -0300

    Add files via upload

---
+++ servlet-test/src/main/java/br/edu/imepac/daos/AlunoDAO.java
@@ -0,0 +1,23 @@
+package br.edu.imepac.daos;
+
+public class AlunoDAO {
+    public void conectarDB(){
+
+    }
+
+    public void desconectarDB(){
+
+    }
+
+    public void atualizar(){
+
+    }
+
+    public void deletar(){
+
+    }
+
+    public void cadastrar(){
+
+    }
+}
